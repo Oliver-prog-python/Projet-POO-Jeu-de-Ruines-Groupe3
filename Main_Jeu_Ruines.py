3a8d3246c1ced1bc5c7366b42b5a9d9e5d8dbb53
Oliver Maamary <olivermaamary@Ordinateur-portable-de-Oliver.local>
2024-11-29 17:38:59 +0100

    Initial commit with Main_Jeu_Ruines.py and Main_Jeu_Units.py

---
+++ Main_Jeu_Ruines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 17:14:34 2024
+
+@author: olivermaamary
+"""
+
---
+++ Main_Jeu_Units.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 17:31:07 2024
+
+@author: olivermaamary
+"""
+
